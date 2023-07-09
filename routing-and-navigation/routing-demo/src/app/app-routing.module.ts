4dc57422d48dadcb84fe3415f68a15cf4969230e
Ladzi <ladziladzani@gmail.com>
2023-07-09 18:45:21 +0200

    implement basic routing

--- routing-and-navigation/routing-demo/src/app/app-routing.module.ts
+++ routing-and-navigation/routing-demo/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DempartmentListComponent } from './dempartment-list/dempartment-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+const routes: Routes = [
+  { path: 'departments', component: DempartmentListComponent},
+  {path: 'employees', component: EmployeeListComponent}
+];
+// path specifies the path in the url (e.g localhost:4200/departments) and this should render the respective component
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [DempartmentListComponent, EmployeeListComponent];
+// for cleaner code, for when importing in the app.module.ts file 
