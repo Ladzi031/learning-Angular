1e7427a7529be3ed499d393d48ab11098080d4ac
Ladzi <ladziladzani@gmail.com>
2023-07-09 14:55:32 +0200

    move to file

---
+++ ngBasics/src/app/testservice.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { StrangePeople } from '/src/interfaces/StrangePeople';
+import { Observable } from 'rxjs';
+import { catchError, map } from  "rxjs/operators";
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+ 
+export class TestserviceService {
+  public url = "/assets/strangePeople.json";
+
+  // DI with typeScript shortcut....
+  constructor(private http: HttpClient) {}
+
+  getPeople(): Observable<StrangePeople[]> {
+    return this.http.get<StrangePeople[]>(this.url).pipe( 
+      map( (data) => {
+        return data
+      }),
+      catchError( (error: HttpErrorResponse) => {
+         return throwError(() => new Error() || "Server Error");
+        }));
+  }  
+}
