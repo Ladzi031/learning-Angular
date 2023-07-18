08f74aaf2dbaacff5fb829f7352dcd93a5aec390
Ladzi <ladziladzani@gmail.com>
2023-07-18 20:32:39 +0200

    added a new component

--- routing-and-navigation/routing-demo/src/app/app.module.ts
+++ routing-and-navigation/routing-demo/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 
 // note: routingComponents...
 @NgModule({
   declarations: [
     AppComponent, 
-    routingComponents
+    routingComponents, DepartmentDetailComponent
   ],
   imports: [
     BrowserModule,
