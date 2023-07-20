02d21b0c3df2c8dd930238e22776af51bb115308
Ladzi <ladziladzani@gmail.com>
2023-07-20 17:18:45 +0200

    added css to department-list component

--- routing-and-navigation/routing-demo/src/app/dempartment-list/dempartment-list.component.ts
+++ routing-and-navigation/routing-demo/src/app/dempartment-list/dempartment-list.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+ interface Test {
+  id: number,
+  name: string
+ }
 @Component({
   selector: 'app-dempartment-list',
   templateUrl: './dempartment-list.component.html',
@@ -7,11 +12,22 @@ import { Component } from '@angular/core';
 })
 export class DempartmentListComponent {
   public departments = [
-    {id: 1, name: "angular"},
-    {id: 2, name: "mongoDB"},
-    {id: 3, name: "python"},
-    {id: 5, name: "java"},
-    {id: 6, name: "node"}
+    {"id": 1, "name": "angular"},
+    {"id": 2, "name": "mongoDB"},
+    {"id": 3, "name": "python"},
+    {"id": 5, "name": "java"},
+    {"id": 6, "name": "node"}
   ]
+
+
+constructor(private router: Router){
+}
+
+  // goal is to display the selected item in a different view component!
+  public onSelect(department: Test) {
+    // get the routing service first (import it)
+    // provide navigate method with all the info it needs to construct the URL!
+    this.router.navigate(['/departments', department["id"]]);
+  }
 
 }
--- routing-and-navigation/routing-demo/src/app/department-detail/department-detail.component.ts
+++ routing-and-navigation/routing-demo/src/app/department-detail/department-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   templateUrl: './department-detail.component.html',
   styleUrls: ['./department-detail.component.css']
 })
-export class DepartmentDetailComponent {
+export class DepartmentDetailComponent implements OnInit {
+
+
+  public departmentId: number = 0;
+  constructor(private route: ActivatedRoute){
+    // this service will provide the instance of the url directly
+  }
+
+  ngOnInit() {
+    // get the current url and get the id param!
+    //let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    //this.departmentId;
+  }
 
 }
