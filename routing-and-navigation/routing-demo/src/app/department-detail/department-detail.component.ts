3382b4f9ba85ec9ed0d4147ab67c93dacfd6cfe7
Ladzi <ladziladzani@gmail.com>
2023-07-21 05:21:16 +0200

    improved reading and getting of route parameters

--- routing-and-navigation/routing-demo/src/app/department-detail/department-detail.component.ts
+++ routing-and-navigation/routing-demo/src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -7,18 +7,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./department-detail.component.css']
 })
 export class DepartmentDetailComponent implements OnInit {
-
-
   
   public departmentId: number = 0;
-  constructor(private route: ActivatedRoute){
+
+
+  constructor(private route: ActivatedRoute, private router: Router){
     // this service will provide the instance of the url directly
   }
 
   ngOnInit() {
     // get the current url and get the id param!
+    /*
     let id = this.route.snapshot.paramMap.get('id') as string;
     this.departmentId = parseInt(id);
+    */
+
+
+    // this way is a little better, as the param gets updated, we also get the updated value! unlike the previous one
+    this.route.paramMap.subscribe((param: ParamMap) => {
+      this.departmentId = parseInt(param.get('id') as string);
+    })
+  }
+
+  onPrevious(){
+    this.router.navigate(["/departments", this.departmentId - 1]);
+  }
+  onNext(){
+    this.router.navigate(["/departments", this.departmentId + 1]);
   }
 
 }
