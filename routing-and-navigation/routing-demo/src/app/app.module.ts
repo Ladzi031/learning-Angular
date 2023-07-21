5e0777357011cbd7313ceed6a056fe218ded376f
Ladzi <ladziladzani@gmail.com>
2023-07-21 15:25:45 +0200

    add new components to demostrate child-routes

--- routing-and-navigation/routing-demo/src/app/app.module.ts
+++ routing-and-navigation/routing-demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
 
 
 // note: routingComponents...
 @NgModule({
   declarations: [
     AppComponent, 
-    routingComponents
+    routingComponents, DepartmentOverviewComponent, DepartmentContactComponent
   ],
   imports: [
     BrowserModule,
